e81ee20d4e75b9518f88f4a57397f477fe2224cc
ymd65536 <ymd65536@gmail.com>
2023-09-16 00:14:01 +0900

    jupyter_config

---
+++ jupyter_config.py
@@ -0,0 +1,16 @@
+# Configuration file for lab.
+
+root_dir = './home/jovyan/'
+# 17fdfbeafa171980754c9ec1917436998c7410ae777ee85a
+c.ServerApp.token = '17fdfbeafa171980754c9ec1917436998c7410ae777ee85a'
+c.ServerApp.port = 8888
+c.ServerApp.open_browser = False
+c.ServerApp.allow_remote_access = True
+c.ServerApp.allow_origin = '*'
+c.ServerApp.ip='0.0.0.0'
+c.ServerApp.notebook_dir = root_dir
+c.ServerApp.allow_root = True
+
+# docker build -t jupyter-nb . --no-chache
+# docker run -it jupyter-nb bash
+# docker run -itd -p 8888:8888 jupyter-nb bash
